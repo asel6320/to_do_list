e8e8d66d49882a82c6cd1855b5043611914989c4
Asel Amatova <asel6320@gmail.com>
2024-06-24 03:09:25 -0700

    created ToDoList in database

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from webapp.models import ToDoList
+
 # Register your models here.
+class ToDoListAdmin(admin.ModelAdmin):
+    list_display = ['id', 'description', 'status', 'deadline']
+    list_display_links = ['id', 'description']
+    list_filter = ['status']
+    search_fields = ['status', 'description']
+    fields = ['description', 'status', 'deadline']
+    readonly_fields = ['deadline']
+
+admin.site.register(ToDoList, ToDoListAdmin)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class ToDoList(models.Model):
+    description = models.TextField(max_length=500, null=False, blank=False, verbose_name="Description")
+    status = models.CharField(max_length=50, null=False, blank=False, verbose_name="Status", default='new')
+    deadline = models.DateField(auto_now=False, verbose_name="Deadline")
+
+    def __str__(self):
+        return f"{self.pk}. {self.description}: {self.status} - {self.deadline}"
+
+    class Meta:
+        db_table = 'toDoList'
+        verbose_name = 'ToDoTask'
+        verbose_name_plural = "ToDoList"
